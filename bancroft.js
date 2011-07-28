19717d1164a51e4eb40dc843c0f706d873a4717f
Pascal Deschênes <pdeschen@rassemblr.com>
2011-07-28 08:47:52 -0400

    add support for gpsd spawning and device hot swapping + winston logger

--- bancroft.js
+++ bancroft.js
@@ -1,8 +1,11 @@
 var spawn = require('child_process').spawn, sys = require('sys'), events = require('events'), fs = require('fs'), net =
-  require('net');
+  require('net'), path = require('path'), winston = require('winston');
+
+var emitter;
 
 /**
  * gpsd client constructor. Connection with daemon is established upon object
+ * 
  * creation.
  * 
  * @param options
@@ -12,6 +15,7 @@ var spawn = require('child_process').spawn, sys = require('sys'), events = requi
  */
 var Bancroft =
   function (options) {
+
     this.satellites = {};
     /* geographic coordinate reference system (long, lat, alt) */
     this.location = {
@@ -21,91 +25,158 @@ var Bancroft =
       altitude : 0,
       speed : 0,
       geometries : {
-        "type" : "Point",
-        "coordinates" : [ 0.0, 0.0, 0.0 ]
+        type : "Point",
+        coordinates : [ 0.0, 0.0, 0.0 ]
       }
     };
 
-    (function () {
-      var self = this;
-      var emitter = events.EventEmitter.call(this);
-      var opts = mixin(options, {
-        'port' : 2947,
-        'hostname' : 'localhost'
-      });
-      var serviceSocket = new net.Socket();
-      serviceSocket.setEncoding('ascii');
-      serviceSocket.on("data", function (payload) {
-        var info = payload.split('\n');
-        for ( var index = 0; index < info.length; index++) {
-          if (info[index]) {
-            try {
-            var data = JSON.parse(info[index]);
-            }
-            catch(error) {
-              emitter.emit('error', {message: "bad message format", cause: info[index], error: error});
-              continue;
-            }
-            if (data.class === 'VERSION') {
-              emitter.emit('connect', {
-                'release' : data.release,
-                'version' : data.version
-              });
-            } else if (data.class === 'TPV') {
-              /* timestamp received is in seconds */
-              self.location.timestamp = data.time * 1000;
-              /* are we moving */
-              if (self.location.latitude !== data.lat || self.location.longitude !== data.lon
-                || self.location.altitude !== data.alt) {
-                self.location.latitude = data.lat;
-                self.location.longitude = data.lon;
-                self.location.altitude = data.alt;
-                self.location.speed = data.speed;
-                self.location.geometries.coordinates = [ data.lon, data.lat, data.alt ];
-                emitter.emit('location', self.location);
-              }
+    var opts = mixin(options, {
+      port : 2947,
+      hostname : 'localhost',
+      gpsd : {
+        spawn : true,
+        device : '/dev/ttyUSB',
+        port : 2947,
+        pid : '/tmp/gpsd.pid'
+      }
+    });
 
-            } else if (data.class === 'SKY') {
-              for ( var index = 0; index < data.satellites.length; index++) {
-                var satellite = data.satellites[index];
-                if (self.satellites[satellite.PRN] !== undefined) {
-                  /* emit if present and new state */
-                  if (self.satellites[satellite.PRN] != satellite.used) {
+    emitter = events.EventEmitter.call(this);
+
+    var go =
+      function () {
+        var self = this;
+
+        var serviceSocket = new net.Socket();
+        serviceSocket.setEncoding('ascii');
+        serviceSocket.on("data", function (payload) {
+          var info = payload.split('\n');
+          for ( var index = 0; index < info.length; index++) {
+            if (info[index]) {
+              try {
+                var data = JSON.parse(info[index]);
+              } catch (error) {
+                winston.error("bad message format", info[index], error);
+                emitter.emit('error', {
+                  message : "bad message format",
+                  cause : info[index],
+                  error : error
+                });
+                continue;
+              }
+              if (data.class === 'VERSION') {
+                emitter.emit('connect', {
+                  'release' : data.release,
+                  'version' : data.version
+                });
+              } else if (data.class === 'TPV') {
+                /* timestamp received is in seconds */
+                self.location.timestamp = data.time * 1000;
+                /* are we moving */
+                if (self.location.latitude !== data.lat || self.location.longitude !== data.lon
+                  || self.location.altitude !== data.alt) {
+                  self.location.latitude = data.lat;
+                  self.location.longitude = data.lon;
+                  self.location.altitude = data.alt;
+                  self.location.speed = data.speed;
+                  self.location.geometries.coordinates = [ data.lon, data.lat, data.alt ];
+                  emitter.emit('location', self.location);
+                }
+
+              } else if (data.class === 'SKY') {
+                for ( var index = 0; index < data.satellites.length; index++) {
+                  var satellite = data.satellites[index];
+                  if (self.satellites[satellite.PRN] !== undefined) {
+                    /* emit if present and new state */
+                    if (self.satellites[satellite.PRN] != satellite.used) {
+                      self.satellites[satellite.PRN] = satellite.used;
+                      emitter.emit('satellite', self.satellites[satellite.PRN]);
+                    }
+                  } else {
+                    /* emit if not present */
                     self.satellites[satellite.PRN] = satellite.used;
                     emitter.emit('satellite', self.satellites[satellite.PRN]);
                   }
-                } else {
-                  /* emit if not present */
-                  self.satellites[satellite.PRN] = satellite.used;
-                  emitter.emit('satellite', self.satellites[satellite.PRN]);
                 }
-              }
 
-            } else if (data.class === 'ERROR') {
-              emitter.emit('error', data);
+              } else if (data.class === 'ERROR') {
+                winston.error('protocol error', data);
+                emitter.emit('error', data);
+              }
             }
           }
-        }
-      });
-      serviceSocket.on("close", function (err) {
-        emitter.emit('disconnect', err);
+        });
+        serviceSocket.on("close", function (err) {
+          winston.info('socket disconnect');
+          emitter.emit('disconnect', err);
+        });
+
+        serviceSocket.on('connect', function (socket) {
+          serviceSocket.write('?WATCH={"enable":true,"json":true}\n');
+          serviceSocket.write('?POLL;\n');
+        });
+
+        serviceSocket.on('error', function (error) {
+          if (error.code === 'ECONNREFUSED') {
+            winston.error('socket connection refused');
+            emitter.emit('error.connection');
+          } else {
+            winston.error('socket error', error);
+            emitter.emit('error', error);
+          }
+        });
+        serviceSocket.connect(opts.port, opts.hostname);
+
+      };
+    go.apply(this);
+    this.on('error.connection', function () {
+      gpsd(opts.gpsd, function () {
+        go.apply(this)
       });
 
-      serviceSocket.on('connect', function (socket) {
-        serviceSocket.write('?WATCH={"enable":true,"json":true}\n');
-        serviceSocket.write('?POLL;\n');
-      });
-
-      serviceSocket.on('error', function (error) {
-        emitter.emit('error', error);
-      });
-      serviceSocket.connect(opts.port, opts.hostname);
-
-    }).apply(this);
+    });
 
     /* only satellites and location properties are externally visible */
     return (this);
   };
+var gpsd = function (options, callback) {
+  if (options.spawn) {
+
+    path.exists(options.device, function (exists) {
+      var arguments = [];
+      /* fg process */
+      arguments.push('-N');
+      arguments.push('-P');
+      arguments.push(options.pid);
+      arguments.push('-S');
+      arguments.push(options.port);
+      arguments.push(options.device);
+      var gpsd;
+      var p = function (callback) {
+        var self = this;
+        if (self.gpsd === undefined) {
+          winston.info('spawning gpsd.');
+          self.gpsd = spawn('gpsd', self.arguments);
+          callback.call();
+          self.gpsd.on('exit', function (code) {
+            winston.error('gpsd died.');
+            self.gpsd = undefined;
+            emitter.emit('error.connection');
+          });
+        }
+      };
+      if (exists) {
+        p.apply(this, [ callback ]);
+      } else {
+        fs.watchFile(options.device, function (curr, prev) {
+          winston.info("device status changed.");
+          p.apply(this, [ callback ]);
+        });
+      }
+    });
+
+  }
+};
 
 var mixin = function (source, destination) {
 
